3c9968d02f2e33c8b0619f76f260420dc4129ee3
The ZZ <ztimes2@pm.me>
2021-03-13 16:53:51 +0000

    Send message to Slack channel

--- main.go
+++ main.go
@@ -1,11 +1,24 @@
 package main
 
 import (
+	"os"
+
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/slack-go/slack"
 )
 
 func main() {
-	lambda.Start(func() (string, error) {
-		return "Hello, world!", nil
+	slackAPIToken := os.Getenv("SLACK_API_TOKEN")
+	slackChannelID := os.Getenv("SLACK_CHANNEL_ID")
+
+	lambda.Start(func() error {
+		slackClient := slack.New(slackAPIToken)
+
+		_, _, err := slackClient.PostMessage(
+			slackChannelID,
+			slack.MsgOptionText("Hello, world!", false),
+		)
+
+		return err
 	})
 }
